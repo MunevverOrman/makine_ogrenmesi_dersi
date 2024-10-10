568413d1fd97e262741b74e9b03e7ed4d74fc453
MunevverOrman <munevverorman1@gmail.com>
2024-10-10 21:22:53 +0300

    motorların silindirine göre fiyat tahmini

--- basic_classification.py
+++ basic_classification.py
@@ -1,7 +1,24 @@
-class BasicClassification():
-    def __init__(self) -> None:
-        pass
-    def fit(train_data):
-        pass
-    def predict(sample):
-        pass
+from sklearn.neighbors import KNeighborsRegressor
+import numpy as np
+
+class BasicClassification:
+    def _init_(self, n_neighbors=3):  
+        self.n_neighbors = n_neighbors
+        self.model = KNeighborsRegressor(n_neighbors=self.n_neighbors)
+    
+    def fit(self, train_data, train_labels):
+        self.model.fit(np.array(train_data).reshape(-1, 1), train_labels)
+    
+    def predict(self, sample):
+        return self.model.predict(np.array(sample).reshape(-1, 1))
+
+train_data = [4, 6, 8, 4, 6, 8, 8] 
+train_labels = [20000, 25000, 30000, 21000, 26000, 31000, 29500] 
+
+knn_classifier = BasicClassification(n_neighbors=3)
+knn_classifier.fit(train_data, train_labels)
+
+silindir_sayisi = int(input("Tahmin etmek istediğiniz motorun silindir sayısını girin: "))
+
+predicted_price = knn_classifier.predict([silindir_sayisi])
+print(f"{silindir_sayisi} silindirli motor için tahmin edilen fiyat: {predicted_price[0]:.2f}")
